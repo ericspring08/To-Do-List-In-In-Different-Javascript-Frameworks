8403670c4dc210219423cf5a4ace69cd61641a8b
Eric Zhang <ericspring08@gmail.com>
2022-07-29 17:16:57 -0500

    Add Svelte

---
+++ svelte/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+import { each } from "svelte/internal"
+
+  let items = [];
+  let item_value = "";
+  function handleClick() {
+    items = [item_value, ...items]
+  }
+</script>
+
+<main>
+  <h1>Svelte</h1>
+  <input id="input" bind:value={item_value} placeholder="enter item name"/>
+  <button on:click={handleClick}>Click to change text</button>
+  {#each items as item, index}
+    <p>{index+1}. {item}</p>
+  {/each}
+</main>
+
+<style>
+  main {
+    text-align: center;
+  }
+</style>
